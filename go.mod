5c4ed710abcb974e0df9ea83bafb152239d71ddf
smzgl <smzgl@163.com>
2022-11-07 14:27:39 +0800

    change packge to github.com/smzgl/go-enum for go install

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/abice/go-enum
+module github.com/smzgl/go-enum
 
 go 1.18
 
--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/abice/go-enum/generator"
+	"github.com/smzgl/go-enum/generator"
 	"github.com/labstack/gommon/color"
 	"github.com/urfave/cli/v2"
 )
